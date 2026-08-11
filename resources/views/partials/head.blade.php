90211b845d0ab09826bd05eb9a078f0d33e8343e
Bupe Katongo <bupe@Piko-Tech-MacBook-Pro.local>
2026-08-11 23:04:46 +0200

    Load Vite production assets in dashboard layout

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,4 +8,6 @@
 <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
 @stack('styles')
